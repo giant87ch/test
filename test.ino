693a1784bea5137f87c975ea09c408a83ae18f38
giant87ch <giant87ch@gmail.com>
2020-11-15 15:02:37 +0900

    送信成功

--- test.ino
+++ test.ino
@@ -1,17 +1,30 @@
 #include "FlexCAN_T4.h"
 
 FlexCAN_T4<CAN0, RX_SIZE_256, TX_SIZE_16> can;
+CAN_message_t buf;
 
 void setup() {
   // put your setup code here, to run once:
- pinMode(14,OUTPUT);
+ pinMode(13,OUTPUT);
  can.begin();
+ can.setBaudRate(1000000);
+ buf.id = 0x1b;
+ buf.len = 8;
+ buf.buf[0] = 0x13;
+ buf.buf[1] = 0x13;
+ buf.buf[2] = 0x13;
+ buf.buf[3] = 0x13;
+ buf.buf[4] = 0x13;
+ buf.buf[5] = 0x13;
+ buf.buf[6] = 0x13;
+ buf.buf[7] = 0x13;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(14,LOW);
+  digitalWrite(13,LOW);
   delay(50);
-  digitalWrite(14,HIGH);
+  digitalWrite(13,HIGH);
   delay(50);
+  can.write(buf);
 }
