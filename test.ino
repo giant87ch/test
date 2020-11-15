389f2ed05d2c0ec8809452d78b71a9bbd009a379
giant87ch <giant87ch@gmail.com>
2020-11-15 19:44:28 +0900

    11/15 19:45 分時点で最新

--- test.ino
+++ test.ino
@@ -7,11 +7,33 @@ int motorCurrent[4] = {0};
 // CAN_message_t buf;
 // CAN_message_t msg;
 
+void setMotorRpm(int id ,int targetRpm){
+  int p = 25;//set GAIN
+  motorCurrent[id] = p*(targetRpm-motor.wEscData[id].rotation);
+  motorCurrent[id] = constrain(motorCurrent[id],-10000,10000);
+}
+
+void setMotorPos(int id ,int targetPos){
+  int p = 2;//set GAIN
+  int rpm = 0;
+  rpm = p*(targetPos-motor.exEscData[id].pos);
+  setMotorRpm(id,rpm);
+}
+
+
 void timerInt(){
+  // motor.getCanData();
+  // Serial.println(motor.wEscData[0].angle);
+  // motorCurrent[0] = 1000;
+  // motor.driveWheel(motorCurrent);
+
   motor.getCanData();
-  Serial.println(motor.wEscData[0].angle);
-  motorCurrent[0] = 1000;
+  // setMotorRpm(0,600);
+  setMotorPos(0,20000);
   motor.driveWheel(motorCurrent);
+  Serial.println(motor.exEscData[0].pos);
+  // Serial.print(", ");
+  // Serial.println(motorCurrent[0]);
 }
 
 void setup() {
