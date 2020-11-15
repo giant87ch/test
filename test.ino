e28c8cf2cc3cd37b2218d81e58fda3108cf53049
giant87ch <giant87ch@gmail.com>
2020-11-15 13:44:56 +0900

    13->14

--- test.ino
+++ test.ino
@@ -1,12 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
- pinMode(13,OUTPUT);
+ pinMode(14,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13,HIGH);
+  digitalWrite(14,HIGH);
   delay(50);
-  digitalWrite(13,LOW);
+  digitalWrite(14,LOW);
   delay(50);
 }
