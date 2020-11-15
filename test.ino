5913a3bf5c9c46cc59c61ebb3a6d111ce500aadb
giant87ch <giant87ch@gmail.com>
2020-11-15 13:18:22 +0900

    highlow

--- test.ino
+++ test.ino
@@ -5,8 +5,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13,HIGH);
-  delay(50);
   digitalWrite(13,LOW);
+  delay(50);
+  digitalWrite(13,HIGH);
   delay(50);
 }
